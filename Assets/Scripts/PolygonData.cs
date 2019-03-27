e39b6d52ddd0806e5c0362616bb4bc181d9e31d3
Yinl <yangshbb@163.com>
2019-03-27 10:37:47 +0800

    fix

--- Assets/Scripts/PolygonData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PolygonData : MonoBehaviour
-{
-    public List<Polygon> ploygons = new List<Polygon>();
-    public int currentPloygon = 0;
-}
